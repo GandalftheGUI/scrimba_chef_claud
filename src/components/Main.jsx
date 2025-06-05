7270c24a06e94f55e4c35c8c3579d4efc07942e7
gandalfthegui <i_remillard@yahoo.com>
2025-06-05 15:43:43 -0700

    remove dupe main

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <>
+      <main>
+            <form className="add-ingredient-form">
+                <input 
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+    </>
+  )
+}
+
+export default App
